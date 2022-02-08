7b21c8316451418093743b7ac9add4da80e72ada
SuperCoder <supercoder@MacBook-Air-von-SuperCoder-2.local>
2022-02-08 13:42:47 +0100

    arithmetic op

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+var x = 20;
+var y = 30;
+console.log(x + y);
+console.log(y - x);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+var z = 10;
+resultOne = (x * y) / z;
+console.log(resultOne);
+
+let a = 15;
+let b = 9;
+console.log(a % b);
+
+let c = 20;
+var resultTwo = (a + b) * c;
+console.log(resultTwo);
+
+a++
+console.log(a);
+b--
+console.log(b--);
+
+var resultThree = b - a;
+console.log(resultOne % resultTwo);
